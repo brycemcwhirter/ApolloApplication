88902169ea868b44e0f6159e07d4ed19dac74f52
Bryce McWhirter <bryce_mcwhirter2@baylor.edu>
2021-03-26 13:35:06 -0500

    added social media object

---
+++ ApolloApplication/src/main/java/apollo/Objects/SocialMedia.java
@@ -0,0 +1,15 @@
+package apollo.Objects;
+
+public class SocialMedia {
+
+    String platform;
+    String userName;
+    String link;
+
+    public SocialMedia(String platform, String userName, String link) {
+        this.platform = platform;
+        this.userName = userName;
+        this.link = link;
+    }
+
+}
