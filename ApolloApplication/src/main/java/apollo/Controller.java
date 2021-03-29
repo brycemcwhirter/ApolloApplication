94ed21c4926964cafd91e48b43e582e00b7d8c63
Bryce McWhirter <bryce_mcwhirter2@baylor.edu>
2021-03-29 17:36:50 -0500

    oh my lord, almost forgot me comments :)

--- ApolloApplication/src/main/java/apollo/Controller.java
+++ ApolloApplication/src/main/java/apollo/Controller.java
@@ -11,40 +11,56 @@ import java.awt.event.ActionListener;
 /**
  * Controller
  */
-public class Controller implements ActionListener {
+public class Controller {
 
     public static void openPage() {
 
+        // Created New JFrame
         JFrame frame = new JFrame("Apollo");
         frame.setSize(500, 500);
 
+        // Created New JPanel
         JPanel panel = new JPanel();
         frame.add(panel);
 
+        // Added a New JLabel. Don't know if this is necesary.
         JLabel label = new JLabel("Apollo");
         label.setBounds(10, 20, 80, 25);
         panel.add(label);
 
+        // New Database Button w/ Action Listener
         JButton newDatabase = new JButton("New Database");
+        newDatabase.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("New Database");
+
+            }
+        });
         panel.add(newDatabase);
 
+        // Import Database Button w/ Action Listener
         JButton importDatabase = new JButton("Import Database");
+        importDatabase.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Import Database");
+            }
+        });
         panel.add(importDatabase);
 
+        // Showing Frame
+        // Just found this set location relative to
+        // function that will put the frame to the center
+        // every time
+        frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
     }
 
     public static void main(String[] args) {
+        // Just Calling Open Page
         openPage();
         System.out.println("hi");
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        System.out.println(e);
-
     }
 
 }
