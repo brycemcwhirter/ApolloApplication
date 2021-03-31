b9e8f5b8c06553efb9581aae88c105137b7bdf24
Bryce McWhirter <bryce_mcwhirter2@baylor.edu>
2021-03-31 18:15:48 -0500

    added serial id

--- ApolloApplication/src/main/java/apollo/TableDB.java
+++ ApolloApplication/src/main/java/apollo/TableDB.java
@@ -16,6 +16,10 @@ import javax.swing.table.DefaultTableModel;
 
 public class TableDB extends JPanel {
 
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
     static DefaultTableModel model;
 
     public TableDB() {
