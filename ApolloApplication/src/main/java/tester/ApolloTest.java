140d86c4d7c242d0d83b384909b42d5b5bb67b76
MKM <malik_mohamedali1@baylor.edu>
2021-04-26 21:05:26 -0500

    test template added to tester package

---
+++ ApolloApplication/src/main/java/tester/ApolloTest.java
@@ -0,0 +1,19 @@
+package tester;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+
+public class ApolloTest {
+	@Test
+	void initialTest() {
+		
+	}
+}
